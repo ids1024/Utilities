836b4f2e6f91217fd59e5c0abe7d261190ca1ea2
Ian D. Scott <ian@perebruin.com>
2014-08-30 13:52:16 -0700

    ianxenvstart.py: use sane variable names

--- bin/ianxenvstart.py
+++ bin/ianxenvstart.py
@@ -9,13 +9,12 @@ from importlib.machinery import SourceFileLoader
 Popen(("xrdb","-merge",os.path.expanduser("~/.Xresources")))
 
 conf = SourceFileLoader("conf", os.path.expanduser("~/.xinitrc.py")).load_module()
-environment = conf.environments[int(sys.argv[1])]
+name, executable, depends = conf.environments[int(sys.argv[1])]
 
-if environment[2]:
-    if hasattr(environment[2], '__iter__'):
-        for i in environment[2]:
+if depends:
+    if hasattr(depends, '__iter__'):
+        for i in depends:
             i.run()
     else:
-        environment[2].run()
-os.execlp(environment[1], environment[1])
-
+        depends.run()
+os.execlp(executable, executable)
