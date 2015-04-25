8ab4fa56abc4ce1806974235dac0d6579e71024b
Ian D. Scott <ian@perebruin.com>
2015-04-25 15:29:05 -0700

    Run clang-format on code with LLVM style

--- src/printdird/printdird.c
+++ src/printdird/printdird.c
@@ -3,41 +3,40 @@
 #include <limits.h>
 #include <cups/cups.h>
 
-#define BUF_LEN ( sizeof(struct inotify_event) + NAME_MAX + 1 )
+#define BUF_LEN (sizeof(struct inotify_event) + NAME_MAX + 1)
 #define WATCHPATH "/home/ian/.PRINT"
 
-int main(int argc, char *argv[])
-{
-    cups_dest_t *dest;
-    char *printer;
-    int notify = inotify_init();
-    
-    if (inotify_add_watch(notify, WATCHPATH, IN_CLOSE_WRITE) == -1)
-        return 1;
-    if (chdir(WATCHPATH) != 0)
-        return 1;
+int main(int argc, char *argv[]) {
+  cups_dest_t *dest;
+  char *printer;
+  int notify = inotify_init();
 
-    //Get default printer
-    if ((dest = cupsGetNamedDest(NULL, NULL, NULL)) == NULL ) 
-	    return 1;
-    printer = strdup(dest->name);
-    free(dest);
+  if (inotify_add_watch(notify, WATCHPATH, IN_CLOSE_WRITE) == -1)
+    return 1;
+  if (chdir(WATCHPATH) != 0)
+    return 1;
 
-    while (1) {
-        char buf[BUF_LEN];
-        struct inotify_event *event;
-        char *filename;
-        int job_id;
+  // Get default printer
+  if ((dest = cupsGetNamedDest(NULL, NULL, NULL)) == NULL)
+    return 1;
+  printer = strdup(dest->name);
+  free(dest);
 
-        read(notify, buf, BUF_LEN);
-        event = (struct inotify_event *) &buf[0];
-	filename = event->name;
-        if (filename[0] == '.')
-            continue;
+  while (1) {
+    char buf[BUF_LEN];
+    struct inotify_event *event;
+    char *filename;
+    int job_id;
 
-        job_id = cupsPrintFile(printer, filename, filename, 0, NULL);
-        cupsStartDocument(CUPS_HTTP_DEFAULT, printer,
-	                  job_id, NULL, CUPS_FORMAT_AUTO, 1);
-        unlink(filename);
-    }
+    read(notify, buf, BUF_LEN);
+    event = (struct inotify_event *)&buf[0];
+    filename = event->name;
+    if (filename[0] == '.')
+      continue;
+
+    job_id = cupsPrintFile(printer, filename, filename, 0, NULL);
+    cupsStartDocument(CUPS_HTTP_DEFAULT, printer, job_id, NULL,
+                      CUPS_FORMAT_AUTO, 1);
+    unlink(filename);
+  }
 }
--- src/rebuild-epub/rebuild-epub.c
+++ src/rebuild-epub/rebuild-epub.c
@@ -7,78 +7,78 @@
 #include <sys/stat.h>
 #include <errno.h>
 
-int main(int argc, char *argv[])
-{
-    struct archive *inarc;
-    struct archive *outarc;
-    struct archive_entry *entry;
-    char *inname;
-    char *outname;
+int main(int argc, char *argv[]) {
+  struct archive *inarc;
+  struct archive *outarc;
+  struct archive_entry *entry;
+  char *inname;
+  char *outname;
 
-    if (argc == 1) {
-        fprintf(stderr, "usage: rebuild-epub file\n");
-        return 1;
+  if (argc == 1) {
+    fprintf(stderr, "usage: rebuild-epub file\n");
+    return 1;
+  }
+
+  inname = realpath(argv[1], NULL);
+  if (inname == NULL) {
+    perror(NULL);
+    return 1;
+  }
+
+  outname = malloc(strlen(inname) + 5);
+  strcpy(outname, inname);
+  strcat(outname, ".new");
+
+  inarc = archive_read_new();
+  archive_read_support_format_zip(inarc);
+  if (archive_read_open_filename(inarc, inname, sysconf(_SC_PAGESIZE)) !=
+      ARCHIVE_OK) {
+    fprintf(stderr, "%s", archive_error_string(inarc));
+    return 1;
+  }
+
+  outarc = archive_write_new();
+  archive_write_set_format_zip(outarc);
+  if (archive_write_open_filename(outarc, outname) != ARCHIVE_OK) {
+    fprintf(stderr, "%s", archive_error_string(outarc));
+    return 1;
+  }
+
+  while (archive_read_next_header(inarc, &entry) != ARCHIVE_EOF) {
+    void *buff;
+    FILE *file;
+    int size;
+    char *fullpath = strdup(archive_entry_pathname(entry));
+    char *filename = basename(fullpath);
+    char *path = dirname(fullpath);
+
+    if (strcmp(path, "OEBPS/Text") == 0 &&
+        (file = fopen(filename, "rb")) != NULL) {
+      struct stat st;
+
+      stat(filename, &st);
+      size = st.st_size;
+
+      buff = malloc(size);
+      fread(buff, size, 1, file);
+      fclose(file);
+      printf("Replaced %s\n", filename);
+    } else {
+      size = archive_entry_size(entry);
+      buff = malloc(size);
+      archive_read_data(inarc, buff, size);
     }
+    free(fullpath);
+    archive_write_header(outarc, entry);
+    archive_write_data(outarc, buff, size);
+    free(buff);
+  }
 
-    inname = realpath(argv[1], NULL);
-    if (inname == NULL) {
-        perror(NULL);
-	return 1;
-    }
+  archive_read_free(inarc);
+  archive_write_free(outarc);
 
-    outname = malloc(strlen(inname) + 5);
-    strcpy(outname, inname);
-    strcat(outname, ".new");
-
-    inarc = archive_read_new();
-    archive_read_support_format_zip(inarc);
-    if (archive_read_open_filename(inarc, inname, sysconf(_SC_PAGESIZE)) != ARCHIVE_OK) {
-        fprintf(stderr, "%s", archive_error_string(inarc));
-	return 1;
-    }
-
-    outarc = archive_write_new();
-    archive_write_set_format_zip(outarc);
-    if (archive_write_open_filename(outarc, outname) != ARCHIVE_OK) {
-        fprintf(stderr, "%s", archive_error_string(outarc));
-	return 1;
-    }
-
-    while (archive_read_next_header(inarc, &entry) != ARCHIVE_EOF) {
-        void *buff;
-        FILE *file;
-        int size;
-        char *fullpath = strdup(archive_entry_pathname(entry));
-        char *filename = basename(fullpath);
-        char *path = dirname(fullpath);
-
-        if (strcmp(path, "OEBPS/Text") == 0
-            && (file = fopen(filename, "rb")) != NULL) {
-            struct stat st;
-
-            stat(filename, &st);
-	    size = st.st_size;
-
-            buff = malloc(size);
-            fread(buff, size, 1, file);
-            fclose(file);
-            printf("Replaced %s\n", filename);
-        } else {
-            size = archive_entry_size(entry);
-            buff = malloc(size);
-            archive_read_data(inarc, buff, size);
-        }
-        free(fullpath); 
-        archive_write_header(outarc, entry);
-        archive_write_data(outarc, buff, size);
-        free(buff);
-    }
-
-    archive_read_free(inarc);
-    archive_write_free(outarc);
-
-    rename(outname, inname);
-    free(inname);
-    free(outname);
-    return 0;
+  rename(outname, inname);
+  free(inname);
+  free(outname);
+  return 0;
 }
